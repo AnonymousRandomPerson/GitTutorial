8ee66dfff85b47d71e5d407b717084b64efc2897
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2018-02-16 17:48:22 -0500

    First

---
+++ Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Somewhere {
+
+    /// <summary>
+    /// 
+    /// </summary>
+    class LightSwitch : MonoBehaviour {
+
+        public GameObject gameObj;
+
+        /// <summary>
+        /// Updates the object.
+        /// </summary>
+        private void Update() {
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                gameObj.SetActive(!gameObj.activeSelf);
+            }
+        }
+    }
+}
